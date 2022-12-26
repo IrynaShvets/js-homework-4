383fcaba6d40f5ba5b259ef88c1056518b761dbc
IrynaShvets <86491334+IrynaShvets@users.noreply.github.com>
2022-12-26 17:56:32 +0200

    added change

--- eleven.js
+++ eleven.js
@@ -1,4 +1,14 @@
+const input = document.getElementById("inputEleven");
 
+function clearInput() {
+   input.value = "";
+}
+function result() {
+ input.value = eval(input.value);
+}
+function clickButton(value) {
+   input.value += value;
+}
 
 /* 
 11. Создайте простейший кальякулятор. На странице должны быть кнопки от 0 до 9 и знаки + - * / =. 
--- ten.js
+++ ten.js
@@ -1,27 +1,90 @@
 const form = document.getElementById("form10");
 const inputNameId = document.getElementById("inputName");
+const inputEmailId = document.getElementById("inputEmail");
+const inputPasswordId = document.getElementById("inputPassword");
+const inputAgeId = document.getElementById("inputAge");
+const inputConfirmPasswordId = document.getElementById("inputConfirmPassword");
 
-form.addEventListener("submit", (event) => {
+const btnId = document.getElementById("submit");
+
+form.addEventListener("submit", validationForm);
+
+function validationForm(event) {
   event.preventDefault();
-  const { name, email, age, password, confirmPassword } = event.currentTarget;
-
-  if (name.value === "" || email.value === "" || password.value === "" || confirmPassword.value === "") {
-    return console.log("Please fill in all the fields!");
-  }
+  const { name, email, age, password, confirmPassword} = event.currentTarget;
 
   const nameValue = name.value;
   const emailValue = email.value;
-  const passwordValue = password.value;
   const ageValue = age.value;
+  const passwordValue = password.value;
   const confirmPasswordValue = confirmPassword.value;
 
-  if (nameValue.length < 4 || nameValue.length > 20) {
+  if (
+    nameValue.length < 4 ||
+    nameValue.length > 20 ||
+    nameValue.match(/(?=.*[._/\|,])/g)
+  ) {
+  
     inputNameId.insertAdjacentHTML(
       "afterend",
       `<span class="insertedContent">*The login value must be no less than 4 characters and no more than 20 characters and must not contain the characters . _ / \ | ,</span>`
     );
+    inputNameId.style.border = "1px solid red";
+    
+    return false;
+  } 
+  
+   if (
+    !emailValue.match(/(?=.*[a-z])(?=.*[.@])[.@a-z]/g)
+  ) {
+  
+    inputEmailId.insertAdjacentHTML(
+      "afterend",
+      `<span class="insertedContentEmail">*The email value must be no empty and must has format text@text.text.</span>`
+    );
+    inputEmailId.style.border = "1px solid red";
+    
     return false;
   }
+
+  if (+ageValue < 0 || typeof +ageValue !== "number") {
+    
+    inputAgeId.insertAdjacentHTML(
+      "afterend",
+      `<span class="insertedContentAge">*The age value must be no less than 0 and typeof value must be number.</span>`
+    );
+    inputAgeId.style.border = "1px solid red";
+    
+    return false;
+  }
+
+  if (
+    !passwordValue.match(
+      /(?=.*[0-9])(?=.*[A-Z]){6,}/g
+    )
+  ) {
+    inputPasswordId.insertAdjacentHTML(
+      "afterend",
+      `<span class="insertedContentPassword">*Password - must be non-empty, at least 6 characters long, have at least one capital letter and at least one number.</span>`
+    );
+    inputPasswordId.style.border = "1px solid red";
+    
+    return false;
+  } 
+
+  if (
+    !confirmPasswordValue.match(
+      /(?=.*[0-9])(?=.*[A-Z]){6,}/g
+    ) || confirmPasswordValue !== passwordValue
+  ) {
+    inputConfirmPasswordId.insertAdjacentHTML(
+      "afterend",
+      `<span class="insertedContentConfirmPassword">*Confirm Password - must be non-empty and ...</span>`
+    );
+    inputConfirmPasswordId.style.border = "1px solid red";
+    
+    return false;
+  } 
 
   console.log({
     nameValue,
@@ -31,13 +94,45 @@ form.addEventListener("submit", (event) => {
     confirmPasswordValue,
   });
 
+  alert("Registration completed successfully.");
+
   const insertedContent = document.querySelector(".insertedContent");
   if (insertedContent) {
     insertedContent.parentNode.removeChild(insertedContent);
+    inputNameId.style.border = 0;
+    
   }
 
-  event.currentTarget.reset();
-});
+  const insertedContentEmail = document.querySelector(".insertedContentEmail");
+  if (insertedContentEmail) {
+    insertedContentEmail.parentNode.removeChild(insertedContentEmail);
+    inputNameId.style.border = 0;
+  }
+
+  const insertedContentAge = document.querySelector(".insertedContentAge");
+  if (insertedContentAge) {
+    insertedContentAge.parentNode.removeChild(insertedContentAge);
+    inputNameId.style.border = 0;
+  }
+
+  const insertedContentPassword = document.querySelector(
+    ".insertedContentPassword"
+  );
+  if (insertedContentPassword) {
+    insertedContentPassword.parentNode.removeChild(insertedContentPassword);
+    inputPasswordId.style.border = 0;
+  }
+
+  const insertedContentConfirmPassword = document.querySelector(
+    ".insertedContentConfirmPassword"
+  );
+  if (insertedContentConfirmPassword) {
+    insertedContentConfirmPassword.parentNode.removeChild(insertedContentConfirmPassword);
+    inputPasswordId.style.border = 0;
+  }
+
+  event.target.reset();
+}
 
 /* 
 10. Создайте форму для регистрации с полями логин, емейл, возраст, пароль, повтор пароля и кнопка Зарегистрироваться. 
